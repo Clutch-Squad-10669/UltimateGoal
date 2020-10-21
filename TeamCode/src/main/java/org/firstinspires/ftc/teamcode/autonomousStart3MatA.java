1aa225920600b7a263d803ae42e8da10fb4afe4e
shaunsingh <shaunsingh0207@gmail.com>
2020-10-20 20:05:27 -0400

    other two robotics members added the autonomous code for the other 2 start positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousStart3MatA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousStart3MatA.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
-public class autonomousStart3MatA {
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.advanced.PoseStorage;
+
+public class autonomousStart3MatA extends LinearOpMode {
+
+    double anglePheta = 90 - (Math.atan((105/24)));
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Pose2d myPose = new Pose2d(-62, 25, Math.toRadians(0));
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Trajectory trajectoryA1Red = drive.trajectoryBuilder(
+                new Pose2d())
+                .splineTo(new Vector2d(0,60), Math.toRadians(0))
+                .splineTo(new Vector2d(-23,36), Math.toRadians(anglePheta))
+                .splineTo(new Vector2d(10,36), Math.toRadians(0))
+                .build();
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        drive.followTrajectory(trajectoryA1Red);
+
+        PoseStorage.currentPose = drive.getPoseEstimate();
+
+
+    }
+
+
 }

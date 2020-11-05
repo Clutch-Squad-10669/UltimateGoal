9e718abfb5fbf09420531978cb304156e8dbcf0b
shaunsingh <shaunsingh0207@gmail.com>
2020-11-04 19:01:56 -0500

    converted autostart1matc back to java from kotlin taught kotlin to everyone else everyone tried to convert start1mata to kotlin as an excercise

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousStart1MatC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousStart1MatC.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
@@ -21,11 +22,34 @@ public class autonomousStart1MatC extends LinearOpMode {// This line measures ho
 
         /* This is our trajectory. It says that from "myPose" we go to the C target zone,
         then we go to S position and, finally, we park on the white line. */
-        Trajectory trajectoryC1Red = drive.trajectoryBuilder(
-                new Pose2d())
-                .splineTo(new Vector2d(54, -60), Math.toRadians(0))
-                .splineTo(new Vector2d(-23, -36), Math.toRadians(anglePheta))
-                .splineTo(new Vector2d(10, -36), Math.toRadians(0))
+        Trajectory trajectoryC1Red1 = drive.trajectoryBuilder(
+                new Pose2d(-62, -50, Math.toRadians(0)))
+                .splineTo(new Vector2d(52.0, -60.0), 0.0)
+                .build();
+
+        Trajectory trajectoryC1Red2 = drive.trajectoryBuilder(
+                new Pose2d(52.0, -60.0,  Math.toRadians(0)))
+                .lineToSplineHeading(new Pose2d(-20.0, -36.0, -anglePheta))
+                .build();
+
+        Trajectory trajectoryC1Red3 = drive.trajectoryBuilder(
+                new Pose2d(-20.0, -36.0,  Math.toRadians(-anglePheta)))
+                .lineToSplineHeading(new Pose2d(-60.0, -25.0, Math.toRadians(0)))
+                .build();
+
+        Trajectory trajectoryC1Red4 = drive.trajectoryBuilder(
+                new Pose2d(-60.0, -25.0,  Math.toRadians(0)))
+                .splineTo(new Vector2d(-23.0, -36.0), anglePheta)
+                .build();
+
+        Trajectory trajectoryC1Red5 = drive.trajectoryBuilder(
+                new Pose2d(-23.0, -36.0,  Math.toRadians(anglePheta)))
+                .lineToSplineHeading(new Pose2d(54.0, -60.0, Math.toRadians(0)))
+                .build();
+
+        Trajectory trajectoryC1Red6 = drive.trajectoryBuilder(
+                new Pose2d(54.0, -60.0,  Math.toRadians(0)))
+                .splineToConstantHeading(new Vector2d(10.0, -60.0), 0.0)
                 .build();
 
         waitForStart();
@@ -33,7 +57,12 @@ public class autonomousStart1MatC extends LinearOpMode {// This line measures ho
         if (isStopRequested()) return;
 
         //This simply tells the robot to follow the trajectory above
-        drive.followTrajectory(trajectoryC1Red);
+        drive.followTrajectory(trajectoryC1Red1);
+        drive.followTrajectory(trajectoryC1Red2);
+        drive.followTrajectory(trajectoryC1Red3);
+        drive.followTrajectory(trajectoryC1Red4);
+        drive.followTrajectory(trajectoryC1Red5);
+        drive.followTrajectory(trajectoryC1Red6);
 
         /* This saves the coordinates in another program so that the robot can recognize where the coordinates
         are when it sees them again */

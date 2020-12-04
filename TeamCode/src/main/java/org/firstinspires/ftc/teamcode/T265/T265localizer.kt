11bb46d2d2ed15a25e0fff3ed4ecbfce2e369e1f
shaunsingh <shaunsingh0207@gmail.com>
2020-12-03 20:37:00 -0500

    fixed t265 localizer, added conversion between pose2d(Ftclb) and pose2c(rr)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T265/T265localizer.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T265/T265localizer.kt
@@ -5,6 +5,7 @@ import com.acmerobotics.roadrunner.localization.Localizer
 import com.arcrobotics.ftclib.geometry.Rotation2d
 import com.arcrobotics.ftclib.geometry.Transform2d
 import com.arcrobotics.ftclib.geometry.Translation2d
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.ChassisSpeeds
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.spartronics4915.lib.T265Camera
 
@@ -12,9 +13,6 @@ class T265localizer(hardwareMap: HardwareMap) : Localizer {
     val cameraToRobot = Transform2d(Translation2d(12.0, 0.0), Rotation2d(180.0))
     val startingPose: com.arcrobotics.ftclib.geometry.Pose2d = com.arcrobotics.ftclib.geometry.Pose2d(Translation2d(0.0, 0.0), Rotation2d(0.0))
     var T265Camera = T265Camera(cameraToRobot, 0.5, hardwareMap.appContext)
-
-
-
     /**
      * Current robot pose estimate.
      */
@@ -47,3 +45,28 @@ class T265localizer(hardwareMap: HardwareMap) : Localizer {
     }
 
 }
+
+
+fun com.arcrobotics.ftclib.geometry.Pose2d.toRR(): Pose2d = Pose2d(this.translation.y, this.translation.x, this.heading)
+
+fun Pose2d.toFTCLib(): com.arcrobotics.ftclib.geometry.Pose2d = com.arcrobotics.ftclib.geometry.Pose2d(-this.x, -this.y, Rotation2d(this.heading))
+
+fun Double.toRadians(): Double = Math.toRadians(this)
+
+val com.arcrobotics.ftclib.geometry.Pose2d.x: Double
+    get() = this.translation.x
+
+val com.arcrobotics.ftclib.geometry.Pose2d.y: Double
+    get() = this.translation.y
+
+val com.arcrobotics.ftclib.geometry.Pose2d.rotationDeg: Double
+    get() = this.rotation.degrees
+
+val com.arcrobotics.ftclib.geometry.Pose2d.rotationRad: Double
+    get() = this.rotation.radians
+
+fun ChassisSpeeds.toRRPose2d(): Pose2d = Pose2d(this.vxMetersPerSecond, this.vyMetersPerSecond, this.omegaRadiansPerSecond)
+
+fun Pose2d.toInches(): Pose2d = Pose2d(this.x / 0.025400051, this.y / 0.025400051, this.heading)
+
+fun com.arcrobotics.ftclib.geometry.Pose2d.toMeters(): com.arcrobotics.ftclib.geometry.Pose2d = com.arcrobotics.ftclib.geometry.Pose2d(this.x * 0.025400051, this.y * 0.025400051, Rotation2d(this.heading))
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/storage/RRtoFTClib.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/storage/RRtoFTClib.kt
@@ -1,4 +1,10 @@
 package org.firstinspires.ftc.teamcode.storage
 
 class RRtoFTClib {
+    fun toRR() {
+
+    }
+    fun toFTClib() {
+
+    }
 }
